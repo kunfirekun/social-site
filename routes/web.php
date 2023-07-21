9e6f7ede8c5a72f39b4a9faf479b01ee2352743d
kunfirekun <kunfirekun@gmail.com>
2023-07-21 15:08:47 +0300

    authentication pages

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,11 @@ require __DIR__.'/auth.php';
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
